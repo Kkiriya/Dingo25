593a280479dc493eeaa8da011c252599252d6913
May <2385428@cegepmontpetit.ca>
2025-01-18 17:02:43 -0500

    FCT : Tree platforms can be visible and enabled when pressed "R"
    It is invisible and disabled when the button hasn't been pushed

--- dingo25/Scripts/tree.gd
+++ dingo25/Scripts/tree.gd
@@ -1,25 +1,29 @@
-extends RigidBody2D
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	$LeftPlatform.visible = false	
+	$RightPlatform.visible = false
+	$LeftPlatform.get_node("CollisionShape2D").disabled = true
+	$RightPlatform.get_node("CollisionShape2D").disabled = true
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+func _physics_process(delta):
+	if Input.is_action_pressed("WaterTree"):
+		Create_Platform(true)
 #Left == true, Right == false
 func Create_Platform(Left: bool) -> void:
 	#When clicked on "x" on keyboard
 	#Actionner animation 1 fois
 	#Rendre la platforme solide et visible
 	if Left:
-		$Platform.visible = true
-		
+		$LeftPlatform.visible = true		
+		$LeftPlatform.get_node("CollisionShape2D").disabled = false
 	else:
-		$Platform.visible = true
-
+		$RightPlatform.visible = true
+		$RightPlatform.get_node("CollisionShape2D").disabled = false
 	
 	
