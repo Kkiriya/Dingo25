96527eeb5c7e77d4883bae260bcc52f3bf27de7d
Kiri <69686396+Kkiriya@users.noreply.github.com>
2025-01-18 21:41:18 -0500

    added respawn button (r on keyboard or y on controller)

--- dingo25/scripts/level_1.gd
+++ dingo25/scripts/level_1.gd
@@ -1,9 +1,12 @@
 extends Node
-
+@onready var player = get_node("Player")
+@onready var respawnPoint = get_node("RespawnPoint")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if Input.is_action_just_pressed("reset level"):
+		player.position = respawnPoint.location
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
