d4610d06cb4c880660e6c45c2a5384ff1ba77bee
ryan-njoumene <ryannjoumene@gmail.com>
2025-01-19 14:31:16 -0500

    Completion of level_2 to Godot

--- dingo25/Scenes/level/level_2.gd
+++ dingo25/Scenes/level/level_2.gd
@@ -1,6 +1,7 @@
 extends Node
 @export var player : CharacterBody2D
 @export var next_level : Area2D
+@export var respawn : Marker2D
 
 var in_body = 0
 
@@ -15,10 +16,31 @@ func _process(delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if in_body == 1:
 		if Input.is_action_just_pressed("up"):
-			get_tree().change_scene_to_file("res://scenes/level/level_2.tscn")
+			get_tree().change_scene_to_file("res://scenes/level/test.tscn")
 func _on_next_level_body_entered(body: Node2D) -> void:
 	in_body = 1
+	#
 	
 
 func _on_next_level_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	in_body = 0
+
+
+func _on_killzone_body_entered(body: Node2D) -> void:
+	print("test1")
+	player.global_position = respawn.global_position
+
+
+func _on_killzone_1_body_entered(body: Node2D) -> void:
+	player.global_position = respawn.global_position
+	print("test1")
+
+
+func _on_killzone_2_body_entered(body: Node2D) -> void:
+	player.global_position = respawn.global_position
+	print("test1")
+
+
+func _on_killzone_3_body_entered(body: Node2D) -> void:
+	player.global_position = respawn.global_position
+	print("test1")
