a4f44dfd2eda0171c9e7b3a38f5d555590b05fb2
May <2385428@cegepmontpetit.ca>
2025-01-19 17:25:44 -0500

    PROGRESS : 1 tree works

--- dingo25/Scripts/interaction_area.gd
+++ dingo25/Scripts/interaction_area.gd
@@ -1,6 +1,8 @@
 extends Area2D
 class_name InteractionArea
 
+@export var interactionmanagera : Node2D
+
 @export var action_name = "Water the tree"
 var interact: Callable = func():
 	pass
@@ -14,9 +16,9 @@ var interact: Callable = func():
 	#pass
 
 
-func _on_body_entered(_body: Node2D) -> void:
-	InteractionManager.register_area(self)
+func _on_body_entered(body: Node2D) -> void:
+	interactionmanagera.register_area(self)
 
 
-func _on_body_exited(_body: Node2D) -> void:
-	InteractionManager.unregister_area(self)
+func _on_body_exited(body: Node2D) -> void:
+	interactionmanagera.unregister_area(self)
--- dingo25/Scripts/interaction_manager.gd
+++ dingo25/Scripts/interaction_manager.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var player = get_tree().get_first_node_in_group("player")
+@export var player : CharacterBody2D
 @onready var label = $Label
 
 const base_text:String = "[R] to "
@@ -19,13 +19,13 @@ func unregister_area(Area: InteractionArea):
 func _process(delta: float):
 	if active_areas.size() > 0 && can_interact:
 		active_areas.sort_custom(_sort_by_distance_to_player)
-		label.text = base_text + active_areas[0].action_name
-		label.global_position = active_areas[0].global_position
-		label.global_position.y -= 50
-		label.global_position.x -= label.size.x / 2
-		label.show()
-	else:
-		label.hide()
+		#label.text = base_text + active_areas[0].action_name
+		#label.global_position = active_areas[0].global_position
+		#label.global_position.y -= 50
+		#label.global_position.x -= label.size.x / 2
+		#label.show()
+	#else:
+		#label.hide()
 		
 func _sort_by_distance_to_player(area1, area2):
 	var area1_to_player = player.global_position.distance_to(area1.global_position)
