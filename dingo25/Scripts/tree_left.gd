e35dbb2842b466a9b7ea713d968d9043917519c9
May <2385428@cegepmontpetit.ca>
2025-01-21 11:10:41 -0500

    FCT : Tree left fonctionne sur level 1

--- dingo25/Scripts/tree_left.gd
+++ dingo25/Scripts/tree_left.gd
@@ -4,10 +4,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	interaction_area.interact = Callable(self, "Create_Platform")
-	$LeftPlatform.visible = false	
-	$RightPlatform.visible = false
+	$LeftPlatform.visible = false
 	$LeftPlatform.get_node("CollisionShape2D").disabled = true
-	$RightPlatform.get_node("CollisionShape2D").disabled = true
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
@@ -17,7 +15,7 @@ func Create_Platform() -> void:
 	#When clicked on "x" on keyboard
 	#Actionner animation 1 fois
 	#Rendre la platforme solide et visible
-	if $LeftPlatform.visible != true:
+	if !$LeftPlatform.visible:
 		$LeftPlatform.visible = true
 		$LeftPlatform.get_node("CollisionShape2D").disabled = false
 		
