537f22aa84a886a71af758c28e131f4a14b6d8a5
ryan-njoumene <ryannjoumene@gmail.com>
2025-01-20 23:40:19 -0500

    Debug Export_itch.io_version

--- dingo25/Scripts/test_level.gd
+++ dingo25/Scripts/test_level.gd
@@ -9,16 +9,16 @@ func _ready() -> void:
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
-func _input(event: InputEvent) -> void:
+func _input(_event: InputEvent) -> void:
 	if in_body == 1:
 		if Input.is_action_just_pressed("down"):
 			get_tree().change_scene_to_file("res://scenes/level/level_1.tscn")
-func _on_next_level_body_entered(body: Node2D) -> void:
+func _on_next_level_body_entered(_body: Node2D) -> void:
 	in_body = 1
 	
 
-func _on_next_level_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+func _on_next_level_body_shape_exited(_body_rid: RID, _body: Node2D, _body_shape_index: int, _local_shape_index: int) -> void:
 	in_body = 0
